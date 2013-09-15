268412a6151bde82b2eb1909d77543aeec48f3d6
Ellen Gummesson <ellengummesson@gmail.com>
2013-09-15 10:03:49 +0200

    Fix CLI parsing.

--- bin/cli.js
+++ bin/cli.js
@@ -7,14 +7,17 @@ var Plit = require('../lib/app');
 /* CLI */
 
 var argv = process.argv.slice(2),
-    args = argv[0];
+    args = argv[0],
+    num = /^\d+.*/;
 
 if (argv.length === 0) {
   console.log('No arguments were provided!');
 } else {
-  if (args === '--help' || args === '-h') {
-    Plit.help();
-  } else {
+  if (args.match(num)) {
     Plit.init(args);
+  } else if (args === '--version' || args === '-v') {
+    Plit.version();
+  }  else {
+    Plit.help();
   }
 }
